2bcb99c1064fbcd104045c531ca549aa9e1414cb
IKEH1024 <pondau1024@gmail.com>
2024-10-04 16:50:27 +0900

    wip

--- 7_Clima-iOS13/Clima/Controller/FavoritesViewController.swift
+++ 7_Clima-iOS13/Clima/Controller/FavoritesViewController.swift
@@ -8,9 +8,47 @@
 
 import UIKit
 
+struct FavoritedLocationGroup: Identifiable {
+    struct FavoritedLocation: Identifiable {
+        let uuid = UUID()
+        var id: String { uuid.uuidString }
+        var name: String
+    }
+    let uuid = UUID()
+    let name: String
+    var id: String { uuid.uuidString }
+    let locations: [FavoritedLocation]
+}
+
+extension FavoritedLocationGroup {
+    static let exampleData: [FavoritedLocationGroup] = [
+        .init(name: "EU", locations: [
+            .init(name: "ベルリン"),
+            .init(name: "アムステルダム"),
+            .init(name: "ロンドン"),
+        ]),
+        .init(name: "アジア", locations: [
+            .init(name: "東京"),
+            .init(name: "バンコク"),
+        ]),
+        .init(name: "オセアニア", locations: [
+            .init(name: "シドニー"),
+            .init(name: "メルボルン"),
+            .init(name: "ロンドン"),
+        ]),
+        .init(name: "アフリカ", locations: [
+            .init(name: "ケープタウン"),
+        ])
+    ]
+}
+
 class FavoritesViewController: UIViewController {
     
+    @IBOutlet weak var locationsTableView: UITableView!
+    
     // MARK: - Properties
+    
+    private var locationGroups = FavoritedLocationGroup.exampleData
             
     // MARK: - LifeCycle
     
@@ -29,5 +67,34 @@ class FavoritesViewController: UIViewController {
     @objc
     func backViewButtonTapped() {
         navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: -
+
+extension FavoritesViewController: UITableViewDelegate {
+    
+}
+
+extension FavoritesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = locationGroups[section].name
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        locationGroups.count
+    }
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        locationGroups[section].locations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = locationGroups[indexPath.section].locations[indexPath.row].name
+        return cell
     }
 }
