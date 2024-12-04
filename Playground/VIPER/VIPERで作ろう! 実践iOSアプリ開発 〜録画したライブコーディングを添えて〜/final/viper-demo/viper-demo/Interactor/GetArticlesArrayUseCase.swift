ec4d3fd2700e596de75ec14d825797b738f168ac
IKEH1024 <pondau1024@gmail.com>
2024-12-04 12:00:30 +0900

    wip

--- Playground/VIPER/VIPERで作ろう! 実践iOSアプリ開発 〜録画したライブコーディングを添えて〜/final/viper-demo/viper-demo/Interactor/GetArticlesArrayUseCase.swift
+++ Playground/VIPER/VIPERで作ろう! 実践iOSアプリ開発 〜録画したライブコーディングを添えて〜/final/viper-demo/viper-demo/Interactor/GetArticlesArrayUseCase.swift
@@ -9,11 +9,30 @@ import Foundation
 
 class GetArticlesArrayUseCase: UseCaseProtocol {
     func execute(_ parameter: Void, completion: ((Result<[ArticleEntity], Error>) -> ())?) {
-        let res: [ArticleEntity] = [
-            .init(id: 1, userId: 1, title: "タイトル1", body: "本文1"),
-            .init(id: 2, userId: 2, title: "タイトル2", body: "本文2"),
-            .init(id: 3, userId: 3, title: "タイトル3", body: "本文3"),
-        ]
-        completion?(.success(res))
+        let session = URLSession(configuration: .default)
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+        let task = session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion?(.failure(error))
+                }
+                return
+            }
+            guard let data = data,
+                  let decoded = try? JSONDecoder().decode([ArticleEntity].self, from: data)
+            else {
+                let error = NSError(domain: "parse-error", code: 1, userInfo: nil)
+                DispatchQueue.main.async {
+                    completion?(.failure(error))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion?(.success(decoded))
+            }
+        }
+        
+        task.resume()
     }
 }
